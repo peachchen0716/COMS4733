c55183784f6d00b5b167db4171b5bce49a9d076f
Yitao <peachchen0716@gmail.com>
2018-10-04 18:03:31 -0400

    added lab1 function

--- bug2.py
+++ bug2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+import math
 import tf
 from geometry_msgs.msg import Twist, Point, Quaternion
 from sensor_msgs.msg import LaserScan
@@ -12,11 +13,14 @@ class Bug2():
         
         rospy.init_node("bug2", anonymous=False)
 
-        self.g_range_ahead = 1 
+        self.range_ahead = None
+        self.range_min = None
+        self.range_right = None
+        self.range_max = None
         self.scan_sub = rospy.Subscriber('scan', LaserScan, self.scan_callback)
         self.cmd_vel = rospy.Publisher("/cmd_vel_mux/input/navi", Twist, queue_size=5)
-        rate = 10
-        r = rospy.Rate(rate)
+        self.rate = 10
+        self.r = rospy.Rate(self.rate)
 
         # Initialize the tf listener
         self.tf_listener = tf.TransformListener()
@@ -35,49 +39,121 @@ class Bug2():
                         
         x_start = position.x
         y_start = position.y
-        
-        find_mline([x_start, y_start], [10.0, 0.0])
-        print self.mline
+        self.mline = self.find_mline([x_start, y_start], [10.0, 0.0])
+        self.dst_tol = 1e-7
+        self.angle_tol = 1e-3
+        angle = math.atan2(0.0 - y_start, 10.0 - x_start)
 
-        reach_goal = False
-        #reach_goal = True
-        
-        #twist = Twist()
-        #twist.linear.x = 2
-    
-        #for i in range(5):
-        #    self.cmd_vel.publish(twist)
-        #    r.sleep()
-        #twist = Twist()
-        #self.cmd_vel.publish(twist)
-        #rospy.sleep(1) 
+        # turn towards m-line 
+        obj_dst = 0.8
 
-        while not reach_goal:
-            if not encounter_object:
-                self.cmd_vel.publish(twist)             
-                encounter_object = (self.g_range_ahead < 0.8)
-            else:
-                self.cmd_vel.publish(twist)
-                encounter_object = (self.g_range_right > 0) 
-            twist = Twist()
-            if encounter_object:
-                twist.angular.z = 1
-            else:
-                twist.linear.x = 1
+        hit_point = set() 
+        print "start bug2"
+        while True: 
+            encounter_object = (self.range_min < obj_dst)
+            (position, rotation) = self.get_odom()
+            reach_goal = (position.x == 10.0 and position.y == 0.0) 
+            while not reach_goal and not encounter_object:
+                self.translate(0.5)  
+                encounter_object = (self.range_min < obj_dst) 
+            
+            print "found an obstacle"             
+            (position, rotation) = self.get_odom()
+            hit_point.add(position)
+               
+            # turn left 
+            while encounter_object:
+                self.rotate(10.0)
+                print "max distance:", self.range_max
+                encounter_object = math.isnan(self.range_ahead) 
+            
+            # follow boundary 
+            print "start following boundary"
+            while False:
+                self.translate(0.5)
+                # turn until it see the object
+                while math.isnan(self.range_right):
+                    print "turn right"
+                    self.rotate(-5.0)
+                    if self.range_right < obj_dst:
+                        break
+                while math.isnan(self.range_right) == False:
+                    print "turn left"
+                    self.rotate(5.0)
 
+                (position, rotation) = self.get_odom()
+                if self.is_on_mline(position) == True:
+                    print "m-line reached"
+                    break
+                if position in hit_point:
+                    print "impossible"
+            break
         print "done"
+    
+    def translate(self, dist):
+        goal_distance = dist
+        linear_speed = 0.5
+        if dist < 0:
+            linear_speed *= -1
+        linear_duration = goal_distance / linear_speed
+        
+        move_cmd = Twist()        
+        move_cmd.linear.x = linear_speed        
+        ticks = int(linear_duration * self.rate)
+        
+        for t in range(ticks):
+            self.cmd_vel.publish(move_cmd)
+            self.r.sleep()
+        
+        # Stop the robot before the rotation
+        move_cmd = Twist()
+        self.cmd_vel.publish(move_cmd)
+        rospy.sleep(1)
+        
+        # Stop the robot
+        self.cmd_vel.publish(Twist())
+
+    def rotate(self, deg):
+
+        goal_angle = deg * pi / 180.0
+        print goal_angle
+        angular_speed = 0.5
+        if deg < 0:
+            angular_speed *= -1
+        angular_duration = goal_angle / angular_speed
+                
+        move_cmd = Twist()            
+        move_cmd.angular.z = angular_speed
+
+        # Rotate for a time to go 180 degrees
+        ticks = int(angular_duration * self.rate)
+        #ticks = abs(ticks) 
+        for t in range(ticks):           
+            self.cmd_vel.publish(move_cmd)
+            self.r.sleep()
+                
+        # Stop the robot before the next leg
+        move_cmd = Twist()
+        self.cmd_vel.publish(move_cmd)
+        rospy.sleep(1)    
+          
+        # Stop the robot
+        self.cmd_vel.publish(Twist())
 
     def scan_callback(self, msg):
-        self.g_range_ahead = min(msg.ranges) 
-        self.g_range_right = msg.ranges[len(msg.ranges) - 1]
+        self.range_ahead = msg.ranges[len(msg.ranges) / 2]
+        self.range_min = min(msg.ranges) 
+        self.range_max = max(msg.ranges) 
+        self.range_right = msg.ranges[len(msg.ranges) - 1]
 
     def find_mline(self, start, target):
         slope = (target[1] - start[1]) / (target[0] - start[0])
         intercept = target[1] - target[0] * slope
-        self.m_line = (slope, intercept)
+        return (slope, intercept)
 
-    def is_on_mline(self, point):
-        if (point[0] * slope + intercept - point[1] < self.mtolerance):
+    def is_on_mline(self, position):
+        diff = position.x * self.mline[0] + self.mline[1] - position.y 
+        if ( abs(diff) < self.dst_tol):
             return True
         return False
 
@@ -102,3 +178,4 @@ if __name__ == '__main__':
         Bug2() 
     except: 
         rospy.loginfo("Bug2 node terminated.")
+    exit()
