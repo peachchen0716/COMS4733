2f20c02a28c0fe4184766ed2192da9d5eef9614e
peachchen0716 <peachchen0716@gmail.com>
2018-11-17 20:13:53 -0500

    build graph

--- rrt/rrtUtil.py
+++ rrt/rrtUtil.py
@@ -33,7 +33,7 @@ def do_intersect(p1, q1, p2, q2):
         return True; 
     return False
 
-def is_inside(obs, n, p):
+def is_inside(ob, n, p):
     if n < 3:
         return False
     
@@ -43,17 +43,38 @@ def is_inside(obs, n, p):
     
     while True:
         next_i = (i + 1) % n
-        if do_intersect(obs[i], obs[next_i], p, extreme):
-            if orientation(obs[i], p, obs[next_i]) == 0:
-                 return on_segment(obs[i], p, obs[next_i])
+        if do_intersect(ob[i], ob[next_i], p, extreme):
+            if orientation(ob[i], p, ob[next_i]) == 0:
+                 return on_segment(ob[i], p, ob[next_i])
             count += 1
         i = next_i
         if i == 0:
             break
     return count % 2 == 1
 
+
+def build_obs_list(obstacle_path):
+    '''
+        returns a list of obstacles (represented by a list of configs)
+    '''
+    obs = list()
+    with open(obstacle_path) as f:
+        quantity = int(f.readline())
+        for i in xrange(quantity):
+            ob = list()
+            n = int(f.readline())
+            for j in xrange(n):
+                line = f.readline().strip().split(' ')
+                ob.append(config(line[0], line[1]))
+            obs.append(ob)
+    return obs
+
 if __name__ == "__main__":
     print("start test")
+    obs = build_obs_list("world_obstacles.txt")
+    for p in obs[0]:
+        print(p.x + ", " + p.y)
+    '''
     polygon1 = [config(0, 0), config(10, 0), config(10, 10), config(0, 10)] 
     n = len(polygon1)
     p = config(20, 20) 
@@ -62,5 +83,5 @@ if __name__ == "__main__":
     print(is_inside(polygon1, n, p))
     p = config(0, 5)  
     print(is_inside(polygon1, n, p))
-
+    '''
 
